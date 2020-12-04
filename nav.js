2cd3d8358d796ba47746b01c39eaacfedb1dc35b
rileydg <75144992+rileydg@users.noreply.github.com>
2020-12-03 20:40:32 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,6 +1,18 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+/* JavaScript for our site's navigation bar */
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+let menuOpen = false;
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  
+  if(!menuOpen) {
+	  toggleButton.classList.add('open');
+	  navbarLinks.classList.add('open');
+	  menuOpen = true;
+  } else {
+	  toggleButton.classList.remove('open');
+	  navbarLinks.classList.remove('open');
+	  menuOpen = false;
+  }
 })

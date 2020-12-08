e41934668623453b7b220bdd7a9768b193d64878
jmm1214 <75147925+jmm1214@users.noreply.github.com>
2020-12-08 18:51:14 -0500

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,18 +1,15 @@
-/* JavaScript for our site's navigation bar */
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 let menuOpen = false;
 
-toggleButton.addEventListener('click', () => {
-  
-  if(!menuOpen) {
-	  toggleButton.classList.add('open');
-	  navbarLinks.classList.add('open');
-	  menuOpen = true;
+toggleButton.addEventListener("click", () => {
+  if (!menuOpen) {
+    toggleButton.classList.add("open");
+    navbarLinks.classList.add("open");
+    menuOpen = true;
   } else {
-	  toggleButton.classList.remove('open');
-	  navbarLinks.classList.remove('open');
-	  menuOpen = false;
+    toggleButton.classList.remove("open");
+    navbarLinks.classList.remove("open");
+    menuOpen = false;
   }
-})
+});
